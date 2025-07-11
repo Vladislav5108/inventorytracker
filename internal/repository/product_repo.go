658d8e4adae75656c651bd873f29d02c0130618e
Наталья Горшкова <natalagorskova@MacBook-Air-von-Natala.local>
2025-07-12 00:52:25 +0300

    Дописал все методы в product_repo.go есть пару моментов которые не до конца понимаю, а именно wrows, err := r.db.Query(query, CategoryID)  завтра продалжаю!

--- internal/repository/product_repo.go
+++ internal/repository/product_repo.go
@@ -6,15 +6,14 @@ type ProductRepository interface {
 	GetByID(id int) (domain.Product, error)                 // возвращает товар по ID
 	GetALL() ([]domain.Product, error)                      // возвращает список всех товаров
 	GetByCategory(CategoryID int) ([]domain.Product, error) // возращает товары конкретной категории
-	SearchByName(keyword string) ([]domain.Product, error)  // возвращает товары по части названия без учета регистра
 }
 
 type ProductAdminRepository interface {
 	ProductRepository
 
-	Add(product domain.Product) error       // добавляем новый товар
-	UpDate(product domain.Product) error    // обновляем данные товара
-	Archiv(id int) error                    //убераем товар из католога но не из базы
-	Restore(id int) error                   // возвращаем товар после архивации
-	GetArchived() ([]domain.Product, error) //возвращаем список скрытых товаров
+	Add(product domain.Product) (int, error) // добавляем новый товар
+	UpDate(product domain.Product) error     // обновляем данные товара
+	Archiv(id int) error                     //убераем товар из католога но не из базы
+	Restore(id int) error                    // возвращаем товар после архивации
+	GetArchived() ([]domain.Product, error)  //возвращаем список скрытых товаров
 }
